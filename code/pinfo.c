bef14b453c7cd4e63ccce2707fd74f95ad69ac95
codelixir <pahulpreet.singh@research.iiit.ac.in>
2021-09-19 22:09:20 +0530

    fixing pinfo attempt 3

--- code/pinfo.c
+++ code/pinfo.c
@@ -40,36 +40,52 @@ void pinfo(int argc, char *argv[])
     }
     else
     {
-        char *buffer;
-        long int proc_group, term_group;
-        int i = 1;
+        char proc[64][64];
+        fscanf(f_stat, "%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s", proc[0], proc[1], proc[2], proc[3], proc[4], proc[5], proc[6], proc[7], proc[8], proc[9], proc[10], proc[11], proc[12], proc[13], proc[14], proc[15], proc[16], proc[17], proc[18], proc[19], proc[20], proc[21], proc[22], proc[23], proc[24], proc[25], proc[26], proc[27], proc[28], proc[29], proc[30], proc[31], proc[32], proc[33], proc[34], proc[35], proc[36], proc[37], proc[38], proc[39], proc[40], proc[41], proc[42], proc[43], proc[44], proc[45], proc[46], proc[47], proc[48], proc[49], proc[50], proc[51]);
+        strcpy(proc_status, proc[2]);
+        strcpy(proc_memory, proc[22]);
+        int proc_group = strtoi(proc[4]), term_group = strtoi(proc[7]);
 
-        while (i && fscanf(f_stat, "%s", buffer))
-        {
-            switch (i)
-            {
-            case 5:
-                proc_group = strtoi(buffer);
-                break;
-            case 8:
-                term_group = strtoi(buffer);
-                break;
-            case 9:
-                i = -1;
-                break;
-            default:
-                break;
-            }
-            i++;
-        }
         if (proc_group == term_group)
         {
-            plus = true;
+            proc_status[1] = '+';
+            proc_status[2] = 0;
         }
+        else
+        {
+            proc_status[1] = 0;
+        }
+        // char *buffer;
+        // long int proc_group, term_group;
+        // int i = 1;
+
+        // while (i && fscanf(f_stat, "%s", buffer))
+        // {
+        //     switch (i)
+        //     {
+        //     case 5:
+        //         proc_group = strtoi(buffer);
+        //         break;
+        //     case 8:
+        //         term_group = strtoi(buffer);
+        //         break;
+        //     case 9:
+        //         i = -1;
+        //         break;
+        //     default:
+        //         break;
+        //     }
+        //     i++;
+        // }
+        // if (proc_group == term_group)
+        // {
+        //     plus = true;
+        // }
         fclose(f_stat);
     }
 
     // status file
+    /*
     char line_status[256];
     FILE *f_status = fopen(path_status, "r");
 
@@ -119,6 +135,7 @@ void pinfo(int argc, char *argv[])
         }
         fclose(f_status);
     }
+    */
 
     // exe file
     char buffer[1024];
