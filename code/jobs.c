afe2b5e7c419caaacc05244f42bdb3fa6a6c7350
codelixir <pahulpreet.singh@research.iiit.ac.in>
2021-10-08 09:22:21 +0530

    job status

--- code/jobs.c
+++ code/jobs.c
@@ -45,6 +45,7 @@ void jobs(int argc, char *argv[])
 
 void show_jobs(bool running)
 {
+    // TODO: Alphabetical Order
     struct Node *duplicate = bg_proc_list->next;
     while (duplicate)
     {
@@ -63,13 +64,13 @@ void show_jobs(bool running)
         else
         {
             fscanf(f_stat, "%s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s %s", proc[0], proc[1], proc[2], proc[3], proc[4], proc[5], proc[6], proc[7], proc[8], proc[9], proc[10], proc[11], proc[12], proc[13], proc[14], proc[15], proc[16], proc[17], proc[18], proc[19], proc[20], proc[21], proc[22], proc[23], proc[24], proc[25], proc[26], proc[27], proc[28], proc[29], proc[30], proc[31], proc[32], proc[33], proc[34], proc[35], proc[36], proc[37], proc[38], proc[39], proc[40], proc[41], proc[42], proc[43], proc[44], proc[45], proc[46], proc[47], proc[48], proc[49], proc[50], proc[51]);
-            if (proc[2][0] == 'R')
-            {
-                show = running;
-            }
-            else if (proc[2][0] == 'S')
+            if (proc[2][0] == 'T')
             {
                 show = !running;
+            }
+            else
+            {
+                show = running;
             }
 
             fclose(f_stat);
